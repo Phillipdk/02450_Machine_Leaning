4aa1e5c6752cbfe5d87aa3d79c9cb952eee4eaac
Phillipdk <phillipkjeldsen@gmail.com>
2019-09-26 13:56:13 +0200

    Update ex1_5_5_BreastTissue.py

--- ex1_5_5_BreastTissue.py
+++ ex1_5_5_BreastTissue.py
@@ -2,20 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns 
 
 # Load file in
 file_path = './BreastTissue.xls'
 breast_data = pd.read_excel(file_path, sheet_name = "Data", index_col=0)
 
-<<<<<<< Updated upstream
-=======
 sns.pairplot(breast_data, hue="Class")
 plt.show()
 
->>>>>>> Stashed changes
-#del K, i, tiss, tissue_names, file_path
-
-# Show the attributes 
+# Save the attribute_names in a list: ['Class', 'IO', ..., 'P']
+# len(attributesNames) = 10
 attributeNames = np.array(breast_data.columns)
 
 # Isolate the class types
@@ -40,44 +37,7 @@ for pos, val in enumerate(data):
         data = np.delete(data, pos, 0)
 
 # Save to .txt file
-<<<<<<< Updated upstream
 np.savetxt("ordnet_data.csv", data, delimiter=",")
-
-#[(plt.figure(), plt.plot(row)) for row in data.T[0:9]]
-
-# plotting all features against each other
-plt.figure(figsize=(10,10),dpi=100)
-
-# Iterating through the features twice
-for i in range(len(attributeNames)-1):
-    for j in range(len(attributeNames)-1):
-        
-        # Defining the subplots
-        plt.subplot(9,9, i*9 +j +1)
-
-        # plotting the the features
-        plt.plot(data[:,i], data[:,j], '.')
-
-        # Setting a name for the columns
-        if i == 8:
-            plt.xlabel(attributeNames[j+1])
-        #else:
-        #    plt.xticks([])
-        # Setting a name for the rows
-        if j == 0:
-            plt.ylabel(attributeNames[i+1])
-        #else:
-        #    plt.yticks([])
-
-        # Set the x,y limits just above what is displayed
-        plt.xlim(0,data[:,i].max()*1.1)
-        plt.ylim(0,data[:,j].max()*1.1)
-
-
-# Pakker subplot lidt tættere sammen
-#plt.subplots_adjust(left=0.1, bottom=None, right=None, top=None, wspace=0.05, hspace=0.05)
-=======
-#np.savetxt("ordnet_data.csv", data, delimiter=",")
 
 sns.scatterplot(y=data[:,2], x=data[:,3], hue=data[:,0])
 
@@ -101,5 +61,3 @@ plt.scatter(x = scatterplot_data_nr1['I0'],
 plt.xlabel('DATA1')
 plt.ylabel('DATA2')
 plt.title('DATA3', y=1.05)
->>>>>>> Stashed changes
-
